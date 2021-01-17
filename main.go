a6a9715cfe7d3dc4b3fb0fab9bf9060e954d2f87
masimacbook <masi19bw@gmail.com>
2021-01-17 22:41:05 +0900

    Prepare docker environment local and prod

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
 
+func main() {
+	engine := gin.Default()
+	engine.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello world",
+		})
+	})
+	err := engine.Run(":8080")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
