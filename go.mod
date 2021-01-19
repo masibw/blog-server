b28a8b0db41127be7b7c4f117b798811517040d1
masimacbook <masi19bw@gmail.com>
2021-01-19 20:51:13 +0900

    Implements db migration

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/golang-migrate/migrate/v4 v4.14.1
 	gorm.io/driver/mysql v1.0.3
 	gorm.io/gorm v1.20.11
 )
--- main.go
+++ main.go
@@ -2,20 +2,35 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/masibw/blog-server/config"
 	"github.com/masibw/blog-server/database"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/golang-migrate/migrate/v4/source/github"
 )
 
 func main() {
-	var err error
-	engine := gin.Default()
+
+	m, err := migrate.New("file://"+os.Getenv("MIGRATION_FILE"), "mysql://"+config.PureDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = m.Up()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	_, err = database.NewDB()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	engine := gin.Default()
 	engine.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "hello world",
